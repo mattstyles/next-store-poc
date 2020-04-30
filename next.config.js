8f2c66e6e4c67506dc5731c0c6e4d78476e0c5a7
Matt Styles <matt@personalurban.com>
2020-04-30 12:25:36 +0100

    add bundle analyser and preact as plugins

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 
+const compose = require('next-compose-plugins')
 const withAnalyser = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYSE
+  enabled: process.env.ANALYSE === 'true'
+})
+// const withPreact = require('@zeit/next-preact')
+const withPreact = require('./plugins/preactX')({
+  // enabled: process.env.NODE_ENV === 'production'
+  enabled: true
 })
 
 const nextConfig = {}
 
-module.exports = withAnalyser(nextConfig)
+module.exports = compose(
+  [
+    withAnalyser,
+    withPreact
+  ],
+  nextConfig
+)
